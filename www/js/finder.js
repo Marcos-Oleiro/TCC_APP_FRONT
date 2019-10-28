852c83068d7c38102cff92b027f19e12e40e30b9
Marcos <marcos.oleiro@compasso.com.br>
2019-10-28 18:17:10 -0300

    adding range input

--- www/js/finder.js
+++ www/js/finder.js
@@ -1,21 +1,29 @@
 import { GeoLocation } from "./geolocation/index.js";
 
 const divContent = document.querySelector('div.content');
+const divRange = document.querySelector('div#range');
+const rangeValue = document.getElementById("slider");
 document.addEventListener('deviceready', function () {
+
 
     console.clear();
 
     let geoLocation = new GeoLocation();
-
-
-
-
+    
     geoLocation.getPosition()
         .then((position) => {
             localStorage.setItem("latitude", position.coords.latitude);
             localStorage.setItem("longitude", position.coords.longitude);
             console.log(localStorage.getItem("latitude"));
             console.log(localStorage.getItem("longitude"));
+
+
+            rangeValue.addEventListener('change', function(){
+                console.log(this.value);
+                divRange.textContent  = this.value;
+            });
+
+            
 
         })
         .catch((err) => {
@@ -25,3 +33,7 @@ document.addEventListener('deviceready', function () {
 
 
 });
+
+function loadUsers(){
+
+}
