bc4ffbab43d1ed7c8fbbe0dd4dd0e4b2ccead4cc
Marcos <marcos.oleiro@compasso.com.br>
2020-01-07 15:40:22 -0300

    sei lá

--- www/js/config/index.js
+++ www/js/config/index.js
@@ -1,3 +1,3 @@
 // export const HOST = 'http://127.6.6.6';
-export const HOST = 'https://8080-c0732caa-f1e1-4af4-b6f3-d7dea1b92adc.ws-us1.gitpod.io';
+export const HOST = 'https://8080-c0732caa-f1e1-4af4-b6f3-d7dea1b92adc.ws-us02.gitpod.io';
 
